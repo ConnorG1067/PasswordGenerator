3cd05bb9443471b1edc1cb98b6ccfb5effe37129
ConnorG1067 <77849049+ConnorG1067@users.noreply.github.com>
2021-01-27 10:04:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-import random
+import random, string
 import paperclip
 from tkinter import *
 ########## Root ##########
@@ -8,7 +8,7 @@ root.title("Pass Gen")
 root.geometry('240x200')
 
 ########## Variables and Lists ##########
-characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o' 'p', 'q', 
+characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 
 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', 
 '#', '$', '%', '^', '&', '*', '(', ')']
 
@@ -17,11 +17,19 @@ password = " "
 
 
 def generate():
-   
-   for i in range(5):
-      print(password + random.choice(characters))
+   passw = ""
+   for i in range(2):
+      passw += random.choice(characters)
 
-passwordTxt = tk.Label(root, bg='#99C8F7', width=35, height=2,)
+     
+
+
+
+
+
+
+
+passwordTxt = tk.Label(root, bg='#99C8F7', width=35, height=2, text=passw)
 passwordTxt.grid(row=0, column=0)
 
 Bgenerate = tk.Button(root, text="Generate",width=10, height=1, command=generate)
