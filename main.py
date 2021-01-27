42e983275f7db08bd47c4bb73867958dd62107e2
ConnorG1067 <77849049+ConnorG1067@users.noreply.github.com>
2021-01-27 15:12:51 -0500

    Added save password feature

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+########## Moduels ##########
 import tkinter as tk
 import random, string
 import pyperclip
 from tkinter import *
 
-########## Variables and Lists ##########
+########## Variable ##########
 characters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 
 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', 
 '#', '$', '%', '^', '&', '*', '(', ')']
@@ -21,6 +22,11 @@ class application:
    def copytoclip(self):
       pyperclip.copy(self.passw)
 
+
+   def puttodoc(self):
+      file_object = open('PasswordGenerator.txt', 'a')
+      file_object.write(self.passw + "\n")
+
    def __init__(self):
       self.passw = ""
       self.root = tk.Tk()
@@ -32,11 +38,14 @@ class application:
       self.Bgenerate = tk.Button(self.root, text="Generate",width=10, height=1, command=self.generate)
       self.Bgenerate.grid(row=1, column=0)
 
-      self.passlength = tk.Scale(self.root, from_=1, to=18, orient=HORIZONTAL)
-      self.passlength.grid(row=3, column=0)
+      self.passlength = tk.Scale(self.root, from_=1, to=20, orient=HORIZONTAL)
+      self.passlength.grid(row=4, column=0)
 
       self.copy = tk.Button(self.root, text="Copy", width = 10, height= 1, command=self.copytoclip)
       self.copy.grid(row=2, column=0)
+
+      self.txtdoc = tk.Button(self.root, text="Save pass", width = 10, height= 1, command=self.puttodoc)
+      self.txtdoc.grid(row=3, column=0)
 
       self.root.mainloop()
 
